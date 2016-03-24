692469e3f147bc7c6e24d142c7b173f883f9bd28
Kimberly Oleiro <koleiro@gmail.com>
2016-03-24 16:47:32 -0400

    read_instruction method

--- marsrover.rb
+++ marsrover.rb
@@ -7,11 +7,28 @@ class Rover
     @direction = direction
   end
 
-  #def read_instruction
-  #end
+  def read_instruction(instructions) #arguement. it needs instructions
+#loop , iterating through each value of the instructions.
+#later when you get the instructions (gets.chomp) is when you can convert it to any array
+#can convert outside the class
+instructions.each do |value|
 
-  #def move
-  #end
+    if value == "M"
+      self.move
+    elsif value == "R"
+      self.turn(value) #try to understand value.
+    elsif value == "L"   #this is just telling it to turn
+      self.turn(value) #this is prompting its option to turn in a specific way
+    else
+      puts "HELP ME!!! IM STUCK ON MARS!"
+    end
+
+  end
+
+  #is this if statement set up correctly?
+  #does it only have to delegate to more specific behaviour
+
+
 
   #def turn
   #end
@@ -20,6 +37,4 @@ end
 
 rover_one = Rover.new(9,5,"S")
 
-puts rover_one.x_coordinate
-puts rover_one.y_coordinate
-puts rover_one.direction
+rover_one.read_instruction
